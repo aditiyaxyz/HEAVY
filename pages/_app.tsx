21d48799e7828bd4256ba71b980475b99ed223aa
aditiyaxyz <140721706+aditiyaxyz@users.noreply.github.com>
2025-12-25 22:33:19 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 import type { AppProps } from "next/app";
 import HeaderNav from "@/components/HeaderNav";
-import VibesAlbum from "@/components/VibesAlbum"; // 👈 import the floating album
-import "../styles/globals.css"; // keep your global styles
+import VibesAlbum from "@/components/VibesAlbum";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* 👇 Global header */}
       <HeaderNav />
-
-      {/* Page content */}
       <Component {...pageProps} />
-
-      {/* 👇 Global floating translucent album */}
       <VibesAlbum />
     </>
   );
