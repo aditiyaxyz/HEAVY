7430a965905bd33575933c7bc9722f015700fe00
aditiyaxyz <140721706+aditiyaxyz@users.noreply.github.com>
2025-12-25 23:11:23 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
+// HEAVY/pages/_app.tsx
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+// If you have a header/navigation component, keep it imported
 import HeaderNav from "@/components/HeaderNav";
 import VibesAlbum from "@/components/VibesAlbum";
-import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Optional site-wide header */}
       <HeaderNav />
+
+      {/* Render the current page */}
       <Component {...pageProps} />
+
+      {/* Background audio, always loaded */}
       <VibesAlbum />
     </>
   );
