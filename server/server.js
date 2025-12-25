d3aace68f3e3431d00042f3ef420fd0a308f5b02
aditiyaxyz <140721706+aditiyaxyz@users.noreply.github.com>
2025-12-25 17:41:06 +0530

    Delete server directory

--- server/server.js
+++
@@ -1,9 +0,0 @@
-// server/server.js - start the app
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
